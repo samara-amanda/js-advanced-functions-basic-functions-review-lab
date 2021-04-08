54e8705ae1d779ce725c6d151098fb25bc17cbd5
Sam <samara.amanda01@gmail.com>
2021-04-08 02:10:41 -0400

    commit tree 9c10580b3abd299300c91b2f953d69f5c27c2d97

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+function saturdayFun(activity= "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity= "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair="*"){
+    return function(word="special"){
+      return `You are ${flair + word + flair}!`
+    }
+  }
+  
+  const Calculator = {
+    add: function(){ return 1 + 3 },
+    subtract: function(){ return 1 - 3 },
+    multiply: function(){ return 1 * 3 },
+    divide: function(){ return 10 / 5 },
+  }
+  
+  function actionApplyer(int, array){
+    if (array.length === 0){
+      return int 
+    }
+  
+    array.map( func => int = func(int))
+  
+    return int
+  
+  }
+  
